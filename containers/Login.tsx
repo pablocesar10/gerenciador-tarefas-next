97d8d203fca63061432961d2a62677a5b33ecb6e
Pablo César <pablocesar1@gmail.com>
2021-12-20 05:37:21 -0300

    Exibindo Modal - 1

--- containers/Login.tsx
+++ containers/Login.tsx
@@ -3,6 +3,8 @@ import { useState } from "react"
 import { executeRequest } from "../services/api";
 import { LoginRequest } from "../types/LoginRequest";
 import { LoginResponse } from "../types/LoginResponse";
+import {Modal} from 'react-bootstrap';
+// import {CrudModal} from '../components/Modal';
 
 type LoginProps = {
     setToken(s: string) : void
@@ -13,6 +15,40 @@ export const Login : NextPage<LoginProps> = ({setToken}) => {
     const [login, setLogin] = useState('');
     const [password, setPassword] = useState('');
     const [msgError, setError] = useState('');
+
+    // state Modal
+    const [showModal, setShowModal] = useState(false);
+    const [errorMsg, setErrorMsg] = useState('');
+    const closeModal = () => {
+        setShowModal(false);
+        }
+
+    const doSignUp = async () => {
+        setShowModal(true);
+        return   
+/*            <Modal
+            show={showModal}
+            onHide={() => closeModal()}
+
+            className="container-modal">
+                <Modal.Body>
+                    <p>Cadastre-se</p>
+                    {errorMsg && <p className = "error">{errorMsg}</p>}
+                    <input type="text"/>
+                    <input type="text"/>
+                    <input type="text"/>
+                    <input type="text"/>
+                </Modal.Body>
+                <Modal.Footer>
+                    <div className ="button col-12">
+                        <button> Salvar </button>
+                        <span>Calcelar</span>
+                </Modal.Footer>
+                </Modal>
+*/
+        }
+
+    
 
     const doLogin = async () => {
         try {
@@ -47,41 +83,6 @@ export const Login : NextPage<LoginProps> = ({setToken}) => {
         }
     }
 
-    const doSignUp = async () => {
-        try {
-            if (!signUp || !password) {
-                setError('favor preencher os dados');
-                return;
-            }
-
-            setError('');
-
-            const body = {
-                login,
-                password
-            };
-
-            const result = await executeRequest('login', 'POST', body);
-            if(result && result.data){
-                const loginResponse = result.data as LoginResponse;
-                localStorage.setItem('accessToken', loginResponse.token);
-                localStorage.setItem('userName', loginResponse.name);
-                localStorage.setItem('userEmail', loginResponse.email);
-                setToken(loginResponse.token);
-            }
-        } catch (e : any) {
-            if(e?.response?.data?.error){
-                console.log(e?.response);
-                setError(e?.response?.data?.error);
-                return;
-            }
-            console.log(e);
-            setError('Erro ao efetuar login, tente novamente');
-        }
-    }
-
-
-
 
     return (
         <div className="container-login">
@@ -101,9 +102,25 @@ export const Login : NextPage<LoginProps> = ({setToken}) => {
                 <div className="buttons">
                     <button onClick={doLogin}>Login</button>
                     <button onClick={doSignUp}>SignUp</button>
+                    <Modal show={showModal} onHide={() => closeModal()} className="container-modal">
+                        <Modal.Body>
+                            <p>Cadastre-se</p>
+                            {errorMsg && <p className = "error">{errorMsg}</p>}
+                            <input type="text"/>
+                            <input type="text"/>
+                            <input type="text"/>
+                            <input type="text"/>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <div className ="button col-12">
+                                <button> Salvar </button>
+                                <span>Calcelar</span>
+                        </Modal.Footer>
+                    </Modal>
                 </div>
             </div>
-
         </div>
+
     )
 }
+
